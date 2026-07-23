b03d8c8d458f282ffe977e50591ec2872ad772ce
socialdemocracy <haeltor80@gmail.com>
2026-07-22 20:37:42 -0600

    hi
    wow

--- out/html/game.js
+++ out/html/game.js
@@ -594,4 +594,10 @@
     }
     window.pinnedCardsDescription = "Advisor cards - actions are only usable once per 6 months.";
   };
+  document.addEventListener('mousemove', e => {
+    document.querySelectorAll('.mytooltiptext').forEach(el => {
+        el.style.setProperty('--mouse-x', e.clientX + 'px');
+        el.style.setProperty('--mouse-y', e.clientY + 'px');
+    });
+});  
 }());
